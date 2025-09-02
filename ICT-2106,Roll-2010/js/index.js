7863aded044e382bc3209ef25a20ac570619f164
Tin Tin <sathitinni2010@gmail.com>
2025-09-02 16:05:51 +0600

    Update index.js

--- ICT-2106,Roll-2010/js/index.js
+++ ICT-2106,Roll-2010/js/index.js
@@ -3,10 +3,10 @@ function validatePassword(password){
 if(pass.length<8){
     return "invalid pass";
 }
-    const up=/[A-Z]/;
-    const low=/[a-z]/;
-    const digit=/[0-9]/;
-    const special_character=/[!,@,#,$,%]/;
+    const up
+    const low=
+    const digit=
+    const special_character=
 if (up.test(password) && digit.test(password)&& special_character.test(password))
     {
 return "valid password";
@@ -15,4 +15,5 @@ else {
     return "invalid password";
 }
 console.log(validatePassword(abcr5-l));
+
 }
